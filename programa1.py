6e551faad47047d492ad8540afa2ecd27ba27ff3
Fede Luchelli <luchellifederico@gmail.com>
2024-01-02 23:53:35 -0300

    first commit

---
+++ programa1.py
@@ -0,0 +1,17 @@
+# cadena1 = "python"
+
+# SELECCIONAR
+# print(cadena1[1])
+
+# SLICING
+# el primero aparece siempre
+# print(cadena1[1:4:2])
+
+# CAMBIAR LETRA
+# palabra = "palavra"
+# palabra[4] = "b"
+# esto no se puede hacer
+# la manera correcta es hacer un slicing
+# palabra = palabra[0:4] + "b" + palabra[5:]
+# print(palabra)
+
