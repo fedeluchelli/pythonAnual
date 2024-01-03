bb5763b4031a1284b074b36bedc464b652b5ac34
Fede Luchelli <luchellifederico@gmail.com>
2024-01-03 15:44:05 -0300

    2commit

--- programa1.py
+++ programa1.py
@@ -15,22 +15,87 @@
 # palabra = palabra[0:4] + "b" + palabra[5:]
 # print(palabra)
 
-# DESAFÍO NUMEROS DE EQUIPOS
+#1 DESAFÍO NUMEROS DE EQUIPOS
 
 # win = 3
 # tie = 1
 # lose = 0
-ganados = int(input("Ingrese la cantidad de partidos ganados: "))
-empatados = int(input("Ingrese la cantidad de partidos empatados: "))
-perdidos= int(input("Ingrese la cantidad de partidos perdidos: "))
-
+# ganados = int(input("Ingrese la cantidad de partidos ganados: "))
+# empatados = int(input("Ingrese la cantidad de partidos empatados: "))
+# perdidos= int(input("Ingrese la cantidad de partidos perdidos: "))
 # total = int(ganados)*win + int(empatados)*tie + int(perdidos)*lose
 # print(total)
 # print(int(ganados)*3 + int(empatados) + int(perdidos)*0)
 # totalPuntos = ganados*3 + empatados + perdidos*0
 # print("Puntos totales: " + str(totalPuntos))
-
 # optimizado
-print("\nPartidos ganados: " + str(ganados) + "\nPartidos empatados: " + str(empatados) + "\nPartidos perdidos: " + str(perdidos) + "\n\nPuntos totales: " + str(ganados*3+perdidos*0+empatados))
+# print("\nPartidos ganados: " + str(ganados) + "\nPartidos empatados: " + str(empatados) + "\nPartidos perdidos: " + str(perdidos) + "\n\nPuntos totales: " + str(ganados*3+perdidos*0+empatados))
+
+#2 DESAFÍO STRING
+
+# cadena_1 = "versátil"
+# cadena_2 = "Python"
+# cadena_3 = "es un lenguaje"
+# cadena_4 = "de programación"
+# texto = cadena_2 + " " + cadena_3 + " " + cadena_4 + " " + cadena_1
+# print(texto)
+
+#3 DESAFÍO SLICING
+
+# cadena1 = "neuqueN"
+# # para dar vuelta  [::-1]
+# cadena_volteada = cadena1[::-1]
+# print(cadena_volteada)
+
+# num1 = "12"
+# num_volteado = num1[::-1]
+# print(num_volteado)
+
+# cadena = "acitametaM,5.8,otipeP,ordeP"
+# cadena_formateada = cadena[::-1]
+# print(cadena_formateada)
 
 
+# materia = cadena_formateada[19:]
+# nota = cadena_formateada[14:17]
+# apellido = cadena_formateada[6:12]
+# nombre = cadena_formateada[0:5]
+
+# print("Nombre: " + nombre + "\nApellido: " + apellido +"\nNota: " + nota + "\nMateria: " + materia)
+
+# cadena_separada = cadena_formateada.split(',')
+# nombre = cadena_separada[0]
+# apellido = cadena_separada[1]
+# nota = cadena_separada[2]
+# materia = cadena_separada[3]
+
+# print("nombre: " + nombre + "\napellido: " +apellido+"\nmateria: "+materia+"\nnota: "+nota)
+
+# INPUT
+# nombre = input("ingrese nombre: ")
+# print(nombre)
+
+# OPERACIONES ARITMÉTICAS
+# a=2
+# b=4
+# c=a*b
+# print(c)
+# cadena = "Python"
+# print(cadena*2)
+
+# PRIMER PROGRAMA EN PYTHON
+
+# Promedio pesado: (13.3 + 2.10)/15. El peso que de 3 es 13, mientras que el de 10 es 2.
+# el 3 es más importante
+
+nota_1 = float(input("ingrese nota 1: "))
+nota_2 = float(input("ingrese nota 2: "))
+nota_3 = float(input("ingrese nota 3: "))
+
+porcentaje_1 = 0.20
+porcentaje_2 = 0.30
+porcentaje_3 = 0.50
+
+promedio = (nota_1*porcentaje_1) + (nota_2*porcentaje_2) + (nota_3*porcentaje_3)
+
+print("La nota final es:", promedio) 
---
+++ programa2.py
@@ -0,0 +1,42 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # #  LISTA # # # # # # # # # # # # # # # # # # # # # #
+
+# mi_lista = ["e", 2, -11, "Fede", [5,4,8]]
+
+# print(mi_lista[4])
+# sub_lista = mi_lista[4]
+# print(sub_lista[2])
+
+# lista_agregada = mi_lista+[123,456,789]
+
+# print(lista_agregada)
+# lista_agregada[:4]=[]
+
+# APPEND - agrega al último
+# lista_agregada.append([123,1233,12333])
+# lista_agregada[2:-4] =[]
+
+# POP - elimina el último (salvo se designe ubicación a eliminar)
+# lista_agregada.pop(1)
+# print(lista_agregada)
+
+# COUNT - cuenta el número de veces que se repite
+# print(lista_agregada.count(2))
+
+# INDEX - busca un ítem y dice la ubicación (error si no lo encuentra)
+# print(lista_agregada.index(2))
+
+# DESAFÍO LISTAS
+
+lista_1 = [1, 12, 123]
+lista_2 = ["Bye", "Ciao", "Agur", "Adieu"]
+lista_1.append(4566789)
+lista_1.append("Hola Mundo!")
+
+print(lista_1)
+
+lista_2.append("Hola y Adios")
+lista_2.append(5555)
+
+print(lista_2)
+
