db27b3f2bd3b71cf07fb3b8b5341b4c084c25282
Fede Luchelli <luchellifederico@gmail.com>
2024-01-03 00:26:43 -0300

    2commit

--- programa1.py
+++ programa1.py
@@ -15,3 +15,22 @@
 # palabra = palabra[0:4] + "b" + palabra[5:]
 # print(palabra)
 
+# DESAFÍO NUMEROS DE EQUIPOS
+
+# win = 3
+# tie = 1
+# lose = 0
+ganados = int(input("Ingrese la cantidad de partidos ganados: "))
+empatados = int(input("Ingrese la cantidad de partidos empatados: "))
+perdidos= int(input("Ingrese la cantidad de partidos perdidos: "))
+
+# total = int(ganados)*win + int(empatados)*tie + int(perdidos)*lose
+# print(total)
+# print(int(ganados)*3 + int(empatados) + int(perdidos)*0)
+# totalPuntos = ganados*3 + empatados + perdidos*0
+# print("Puntos totales: " + str(totalPuntos))
+
+# optimizado
+print("\nPartidos ganados: " + str(ganados) + "\nPartidos empatados: " + str(empatados) + "\nPartidos perdidos: " + str(perdidos) + "\n\nPuntos totales: " + str(ganados*3+perdidos*0+empatados))
+
+
