e5403bc650611229649670573cd2ea6afba6a52c
Fede Luchelli <luchellifederico@gmail.com>
2024-01-09 10:34:49 -0300

    día 5

--- programa5.py
+++ programa5.py
@@ -123,6 +123,100 @@
 # en cada paso de la iteración se tiene en cuenta a un único elemento
 # del objeto iterable, sobre el cual se pueden aplicar operaciones
 
-lista = [1,2,3,4,5]
-for valor in lista:
-    print("Soy un item de la lista y valgo",valor)
+# lista = [1,2,3,4,5]
+# for valor in lista:
+#     print("Soy un item de la lista y valgo",valor)
+# # se definió como 'valor' a cada item de la lista
+# # que solamente vale para el objeto que está iterando
+
+# lista = [0,1,2,3]
+# for num in lista:
+#     print("Soy un valor de la lista y valgo",num)
+#     num*=5
+#     print("Soy un valor de la lista y ahora valgo", num)
+#     num/=5
+#     print("Y ahora vuelvo a valer", num)
+
+# Modificando la lista
+# indice=0
+# numeros=[6,7,8,9,10,5]
+# for numero in numeros:
+#     numeros[indice]*=5  
+#     indice+=1
+# print(numeros)
+
+# Sentencia For
+
+# texto = "Hola mundo, estoy usando Python"
+# for letra in texto:
+#     print(letra)
+# texto2=""
+# for letra in texto:
+#     texto2=letra*2
+# print(texto2)
+
+
+#       RANGE
+
+# el for necesita colección de datos para poder ser usado,
+# en otros lenguajes necesitamos solamente un número para
+# indicar las iteraciones a cumplir
+
+# para simularloo está la función range() que representa
+# una colección de números inmutables
+
+# Constructores para crear objetos Range
+
+# #   1-  range(fin):
+# #       crea una secuencia que va desde 0 hasta fin -1
+
+# for numero in range(10):
+#     print(numero)
+
+# #   2- range(inicio, fin):
+# #       secuencia numérica que va desde inicio hasta fin -1
+
+# for numero in range(1,5):
+#     print(numero)
+    
+# #   3- range(inicio, fin [paso])
+
+# for numero in range(4,25,2):
+#     print(numero)
+
+#       la ventaja con la lista[] es que ocupa menos espacio en memoria
+#       lista[] lee todo e imprime, mientras que con range() interpreta
+#       inicio-fin y ejecuta. Genera los números sobre la marcha y no
+#       acumulando datos, se ejecuta el 0 y se crea, cuando se ejecuta
+#       el 1 se crea el 1 y se elimina el 0. Así sucesivamente
+
+
+#       FOR-ELSE
+
+# for numero in range(10):
+#     print("numero vale", numero)
+# else:
+#     print("se terminó de iterar y el número vale:",numero)
+    
+#       FOR-BREAK-CONTINUE-PASS
+
+# for numero in range(10):
+#     if numero==2:
+#         continue
+#     elif numero ==8:
+#         break
+#     else:
+#         print("Se terminó de iterar y número vale:", numero)
+        
+#       ENUMERATE
+
+# frutas =["manzana","pera","sandía"]
+# for indice, fruta in enumerate(frutas):
+#     print(f"Índice: {indice}, Fruta: {fruta}")
+
+cancion="Me gusta"
+cosas = ["aviones","viajar","mañana","viento","soñar","la mar"]
+tu=", me gustas tú"
+
+for indice, cosa in enumerate(cosas):
+    print(f"{cancion} {cosa} {tu}")
